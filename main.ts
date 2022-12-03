f122e6f781631f9577629305c2dcdd29c9c79c27
fishiversity <41026299+fishiversity@users.noreply.github.com>
2022-12-02 22:09:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     servos.P1.stop()
     servos.P2.stop()
+    servos.P0.stop()
 })
 input.onButtonPressed(Button.B, function () {
     servos.P2.run(50)
